41a4492484768d2b7af86c015a324c335bfd7fb9
ArliumDev <arliumdev@gmail.com>
2026-04-22 17:41:19 +0100

    CORS middleware added

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_pool
 from routers import users, events, registrations, account
 from middleware.auth import JWTMiddleware
@@ -12,6 +13,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:5173"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+
+)
 app.add_middleware(JWTMiddleware)
 app.include_router(account.router, prefix="/account")
 app.include_router(users.router, prefix="/users")
